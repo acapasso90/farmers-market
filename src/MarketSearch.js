98f4be414727fcb8ea4d91a38f66d9935abc43ab
acapasso <69538713+acapasso90@users.noreply.github.com>
2021-06-06 10:46:27 -0400

    Created React App, added api links
    Created React App, added api documentation link as well as 2 separate API search links for farmers market

---
+++ src/MarketSearch.js
@@ -0,0 +1,8 @@
+//Zip search for IDs
+//https://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=${zip}
+
+//Info search BY the above ID
+// https://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=${id}
+
+//API Documentation
+//https://search.ams.usda.gov/farmersmarkets/v1/svcdesc.html
