628d7e4dab880233966b730488b7f8e11343151f
acapasso <69538713+acapasso90@users.noreply.github.com>
2021-06-07 12:44:10 -0400

    Installed Axios, set up input and zip code state in MarketSearch.js
    Installed Axios, set up input and zip code state in MarketSearch.js

--- src/MarketSearch.js
+++ src/MarketSearch.js
@@ -1,8 +1,42 @@
+import React, {useState} from "react";
+
+export default function MarketSearch(){
+const [zipcode, setZipcode] = useState("71286");
+let zipHolder = null;
+//API Documentation
+//https://search.ams.usda.gov/farmersmarkets/v1/svcdesc.html
+
 //Zip search for IDs
 //https://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=${zip}
 
 //Info search BY the above ID
 // https://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=${id}
 
-//API Documentation
-//https://search.ams.usda.gov/farmersmarkets/v1/svcdesc.html
+function SearchZipcode(){
+
+}
+
+
+function handleSubmit(event){
+    event.preventDefault();
+    setZipcode(zipHolder);
+    SearchZipcode();
+}
+
+function setZip(event){
+    event.preventDefault();
+    zipHolder = event.target.value;
+}
+
+
+return( 
+    <div className="MarketSearch">
+        <form onSubmit={handleSubmit}>
+            <input type="text" className="zipInput" onChange={setZip} placeholder="Enter Zipcode" />
+            <button className="searchButton">Search</button>
+        </form>
+
+    </div>)
+}
+
+
